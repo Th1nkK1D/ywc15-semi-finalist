419bbb7254f34d2f6f469becaab7df038375a740
Withee Poositasai <witheep@gmail.com>
2017-11-25 23:09:15 +0700

    Add basic person card layout

--- src/components/PersonCard.vue
+++ src/components/PersonCard.vue
@@ -1,7 +1,21 @@
 <template>
   <v-card class="ma-1">
     <v-card-text>
-      {{person}}
+      <v-layout row>
+        <v-flex xs3>
+          <v-layout column>
+            <v-flex>
+              <strong>{{person.interviewRef}}</strong>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex>
+          <v-layout column>
+            <v-flex><strong>{{person.firstName}} {{person.lastName}}</strong></v-flex>
+            <v-flex>สัมภาษณ์รอบเช้า</v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
     </v-card-text>
   </v-card>
 </template>
@@ -12,7 +26,7 @@ export default {
   props: ['person'],
   data () {
     return {
-      
+
     }
   }
 }
