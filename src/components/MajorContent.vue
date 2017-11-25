4a15c2267d3db7e10e555e75afdffbc5d6e17d8e
Withee Poositasai <witheep@gmail.com>
2017-11-25 22:47:21 +0700

    Add major content basic layout

--- src/components/MajorContent.vue
+++ src/components/MajorContent.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    {{list}}
-  </div>
+  <v-layout column class="ma-1">
+    <v-layout row>
+      <v-flex xs3>icon</v-flex>
+      <v-flex>description</v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <!-- Card flex -->
+      <v-flex xs3 v-for="item in list" :key="item.interviewRef">
+        <v-card class="ma-1">
+          <v-card-text>
+            {{item}}
+          </v-card-text>
+        </v-card>
+      </v-flex><!-- End of Card flex -->
+    </v-layout>
+  </v-layout>
 </template>
 
 <script>
